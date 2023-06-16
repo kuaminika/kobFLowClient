a81f261657bd62ea9d67f593997f7b6dcf2afcaf
kuaminika <kuaminika@gmail.com>
2023-06-16 10:48:38 -0400

    added KobHolders viewAll

---
+++ KobHolders/index.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<?php 
+ require_once(__DIR__."/../_generics/pageIgniter.php");
+$pageArgs = [];
+$pageArgs["configs"] = $configs;
+$pageArgs["pageName"] = "Kob holders";
+$pageArgs["pageTitle"]= "Kob holders";
+$pageArgs["jsScriptList"]= [ ];
+//TODO modify list.php template to react if not all these variables are not provided
+$pageHelper =\kuaminika\generics\PageIgniter::Ignite($pageArgs);
+ $navTmplt= $pageHelper->getNavigation();
+ $pageName = $pageHelper->getPageName();
+ $pageTitle = $pageHelper->getPageTitle();
+ $generatedThead = ' ';
+ $jsGeneratorScript = ' ';
+ $jsScriptsInclusions = $pageHelper->generateJSScriptTags();
+ $customJsScript =  ' ';?>
+
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+   <!-- <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>-->
+   
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.111.3">
+    <title><?php echo $pageTitle ; ?></title>
+ 
+    
+
+<link href="bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        width: 100%;
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+
+      .btn-bd-primary {
+        --bd-violet-bg: #712cf9;
+        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+
+        --bs-btn-font-weight: 600;
+        --bs-btn-color: var(--bs-white);
+        --bs-btn-bg: var(--bd-violet-bg);
+        --bs-btn-border-color: var(--bd-violet-bg);
+        --bs-btn-hover-color: var(--bs-white);
+        --bs-btn-hover-bg: #6528e0;
+        --bs-btn-hover-border-color: #6528e0;
+        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+        --bs-btn-active-color: var(--bs-btn-hover-color);
+        --bs-btn-active-bg: #5a23c8;
+        --bs-btn-active-border-color: #5a23c8;
+      }
+      .bd-mode-toggle {
+        z-index: 1500;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="dashboard.css" rel="stylesheet">
+  </head>
+  <body>
+ 
+
+    
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Kob flows</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+      <a class="nav-link px-3" href="#">Sign out</a>
+    </div>
+  </div>
+</header>
+
+<div class="container-fluid">
+  <div class="row">
+    <?php  echo $navTmplt->getNavigation($pageName);  ?>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">   
+      <h2 id="titleHolder" >Section title</h2>
+      <div class="table-responsive">
+        <table id="merhcantList" class="table table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Id # </th>
+              <th scope="col">Name</th>
+            </tr>
+          </thead>
+          <tbody  v-for="(merchant, index) in merchants" >
+              <tr><td>{{merchant.id}}</td><td>{{merchant.name}}</td></tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  </div>
+</div>
+
+
+    <script src="app.js"></script>
+    <script>
+        app.load({title :"<?php echo $pageTitle ;?>"})
+    </script>  
+  </body>
+</html>

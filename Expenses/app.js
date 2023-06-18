dbb0844807f1b35f58b4f308acbc0465b91142a2
kuaminika <kuaminika@gmail.com>
2023-06-18 04:15:58 -0400

    updated Expense page

--- Expenses/app.js
+++ Expenses/app.js
@@ -8,10 +8,48 @@ function App(args){
         self.title = props.title ||"Title";
 
     }
+
+    self.filterList = (contextName,entered)=>{
+      console.log("contextName",contextName,self[contextName],"end")
+      let filteringContext = {...self[contextName]};
+      console.log(filteringContext);
+      let data = filteringContext.data;
+      console.log(data,entered);
+      filteringContext.data = data.filter(el=>(el.name.includes(entered)));
+      chooserFeed(filteringContext)
+    }
     self.setArgs(props);
 
     let titleHolderId = props.titleHolderId ||"titleHolder";
-    
+    function chooserFeed(args)
+    {
+      let contextName = args.contextName;
+      let data = args.data;
+      let templateAddress = args.templateAddress;
+
+      let template =  document.querySelector(templateAddress);
+          
+              
+      let listGroup = template.parentElement;
+      
+       const elements = listGroup.getElementsByClassName("chooser-fed");
+        while(elements.length > 0){
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+      let tmpHolder = document.createElement("div");
+      data.forEach(element=>{
+          let rowStr = template.innerHTML;
+          
+          rowStr = rowStr.replace(`{{${contextName}.id}}`,element.id);
+          rowStr = rowStr.replace(`{{${contextName}.name}}`,element.name);
+          tmpHolder.innerHTML = rowStr;
+          let row = tmpHolder.querySelector("div");
+          row.className = row.className+ " chooser-fed";
+       //   console.log(row);
+          listGroup.appendChild(row);
+      });
+
+    }
     self.load = (args)=>{
 
       const { createApp } = Vue
@@ -20,20 +58,58 @@ function App(args){
 
         self.setArgs(args);
         let url = `https://dev.korosol.com/kobFLow/KobFlow/API/index.php?context=Flows&requestAction=getAll&sourceContext=Expense`;
+        let urlMerchants = `https://dev.korosol.com/kobFLow/KobFlow/API/index.php?context=Flows&requestAction=getAll&sourceContext=Merchant`;
+        let urlCategory = `https://dev.korosol.com/kobFLow/KobFlow/API/index.php?context=Flows&requestAction=getAll&sourceContext=ExpenseCategory`;
+        let urlKobHolder = `https://dev.korosol.com/kobFLow/KobFlow/API/index.php?context=Flows&requestAction=getAll&sourceContext=KobHolder`;
+
+        fetch(urlCategory).then(r=>r.json()).then(wrapped=>wrapped.subject).then(data=>{
+          
+          data.sort((a,b)=>(b.id-a.id)); 
+          console.log(data)
+          
+          let map = {};
+          data.forEach(e=>{map[e.id]=e});
+                let contextName = "category";
+                let templateAddress = "#categoryField .list-wrap template ";
+                self.category = {  contextName,data ,templateAddress };
+                chooserFeed(self.category );
+        });
+        
         fetch(url).then(r=>r.json()).then(wrapped=>wrapped.subject).then(data=>{
-        data.sort((a,b)=>(b.id-a.id));
-        data.map(d=>{d.createdDate = d.createdDate.split('T')[0]});
-         console.log(data)
-
-
-          createApp({
-            data() {
-              return {data }
-            }
-          }).mount("#data");
-        })
-
+              data.sort((a,b)=>(b.id-a.id));
+              data.map(d=>{d.createdDate = d.createdDate.split('T')[0]});
+              self.expenses = data;
+              console.log(data)
+                createApp({
+                  data() {
+                    return {data }
+                  }
+                }).mount("#data");
+        });
+        
+        fetch(urlKobHolder).then(r=>r.json()).then(wrapped=>wrapped.subject).then(data=>{
+                data.sort((a,b)=>(b.id-a.id)); 
+                console.log(data)
+                let map = {};
+                 data.forEach(e=>{map[e.id]=e});
+                let contextName = "account";
+                let templateAddress = "#accountField .list-wrap template ";
+                self.account= {  contextName,data ,templateAddress };
+                chooserFeed(self.account);
+                  
+          });
     
+        fetch(urlMerchants).then(r=>r.json()).then(wrapped=>wrapped.subject).then(data=>{
+          data.sort((a,b)=>(b.id-a.id));
+          console.log(data);
+                let map = {};
+                 data.forEach(e=>{map[e.id]=e});
+                let contextName = "merchant";
+                let templateAddress = "#merchantField .list-wrap template ";
+                self.merchant= {  contextName,data ,templateAddress,map };
+                chooserFeed( self.merchant);
+        });
+        
 
         let titleHolder = document.getElementById(titleHolderId);
         titleHolder.innerText = self.title;
--- Expenses/index.php
+++ Expenses/index.php
@@ -1,11 +1,11 @@
 <?php 
  require_once(__DIR__."/../_generics/pageIgniter.php");
-
+ require_once(__DIR__."/../_templates/_partial_listChooser.php");
  $pageArgs = [];
  $pageArgs["configs"] = $configs;
  $pageArgs["pageName"] = "Expenses";
  $pageArgs["pageTitle"]= "Expenses";
- $pageArgs["jsScriptList"]= ["app.js"];
+ $pageArgs["jsScriptList"]= ["app.js?v6"];
 //TODO modify list.php template to react if not all these variables are not provided
 $pageHelper =\kuaminika\generics\PageIgniter::Ignite($pageArgs);
 $navTmplt= $pageHelper->getNavigation();
@@ -16,10 +16,10 @@ $customJsScript = ' app.load({title :"'.$pageTitle.'"})';
 $generatedThead = '<thead>
 <tr>
   <th scope="col">#id</th>
-  <th scope="col">Description</th>
+  <!--<th scope="col">Description</th>-->
   <th scope="col">Merchant  </th>
   <th scope="col">Category</th>
-  <th scope="col">Account</th>
+  <th scope="col">Kob holder</th>
   <th scope="col">Amount</th>
   <th scope="col">Date</th>
 </tr>
@@ -28,7 +28,7 @@ $jsGeneratorScript = '
 <template  v-for="(expense, index) in data" >
     <tr>
       <td>{{expense.id}}</td>
-      <td>{{expense.description}}</td>
+     <!-- <td>{{expense.description}}</td>-->
       <td>{{expense.merchantName}}</td>
       <td>{{expense.categoryName}}</td>
       <td>{{expense.kobHolderName}}</td>
@@ -36,5 +36,53 @@ $jsGeneratorScript = '
       <td>{{expense.createdDate}}</td>
     </tr>
 </template>';
+$form = '
+<div class="form-wrap">
+    <div class="nav  nav-tabs">
+        <div class="nav-item active">
+          <a class="nav-link active" href="#">Edit</a>
+        </div> 
+        <div class="nav-item">
+          <a class="nav-link " href="#">Add</a>
+        </div> 
+    </div>
+    <form >
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="description">Description:</label>
+        <div class="col-sm-12">
+          <textarea  class="form-control"  style="width:100%"></textarea>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="control-label col-sm-2" for="amount">Amount:</label>
+        <div class="col-sm-12">
+          <input type="text" class="form-control" id="amount" placeholder="amount">
+        </div>
+      </div>
+
+
+      <div id="merchantField" class="form-group mt-1">
+      '.generateListChooser(["title"=>"Merchant","contextName"=>"merchant","id"=>"merchantField"]).'
+        
+      </div>
+      <div id="categoryField" class="form-group mt-1">
+      '.generateListChooser(["title"=>"Category","contextName"=>"category","id"=>"categoryField"]).'
+      </div>
+
+      
+      <div id="accountField" class="form-group mt-1">
+      '.generateListChooser(["title"=>"Kob holder","contextName"=>"account","id"=>"accountField"]).'
+      </div>
+
+      <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+      </div>
+    </form>
+</div>
+';
+
+
 
 require_once (__DIR__."/../_templates/list.php");
